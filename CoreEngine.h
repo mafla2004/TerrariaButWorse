7d9063132a74fcec958efbf9a2a5599d7ffc19cd
Mario Flaviano Caridi <mafla.caridi@gmail.com>
2025-10-01 17:32:58 +0200

    Implementations, bug fixing, testing the instantiation of smart pointers using aliases and wrapper functions

--- CoreEngine.h
+++ CoreEngine.h
@@ -12,7 +12,9 @@
 #include <string>
 #include <map>
 
-class Engine
+#define type_assert(base, derived, msg) static_assert(std::is_base_of<base, derived>::value, msg)
+
+namespace Engine
 {
 /*	I planned to make a custom garbage collector, but since this is a learning project I decided
 	to use C++'s smart pointers. I still plan to implement a custom garbage collector
@@ -59,20 +61,24 @@ class Engine
 	// ENGINE BASE CLASSES
 	// --------------------------------------------------
 
+	class Component;
+
 	/*
 	*	Generic object class, for any object the engine might use
 	*/
-	class Object
+	class Object : public std::enable_shared_from_this<Object>
 	{
 	protected:
 		bool bShouldTick = true;
 
-		std::vector<Ownership<class Component>> components;
+		std::vector<Ownership<Component>> Components;
 	public:
 		inline Object() = default;
 		virtual ~Object() = default;
 
-		virtual void tick(float DeltaTime) = 0;
+		virtual void tick(float DeltaTime);
+
+		virtual Pointer<Object> GetSelf() final;
 
 		// Checks if the class already owns a component of a certain class
 		template<class C>
@@ -94,6 +100,8 @@ class Engine
 		Reference<C> GetComponentOfClass();
 	};
 
+	inline Pointer<Object> Object::GetSelf() { return shared_from_this(); }
+
 	/*
 	*  An actor is an object that can be spawned in the game world
 	*/
@@ -107,7 +115,7 @@ class Engine
 		Reference<Actor> Parent;
 		std::vector<Pointer<Actor>> Children;
 	public:
-		virtual ~Actor() = default;
+		virtual ~Actor() override = default;
 	};
 
 	// --------------------------------------------------
@@ -118,10 +126,30 @@ class Engine
 	*	A component is a special object that is made to be attached to other objects.
 	*	Only its parent object can have an explicit pointer to it, this means that it's the
 	*	object's duty to call the tick functions of the components.
+	* 
+	*	Components are very strict, only the parent object can have a unique, concrete
+	*	pointer to them and they are considered part of the parent object themselves,
+	*	therefore they are not counted in the engine's gameobjects collection.
 	*/
 	class Component : public Object
+	{ 
+	friend class Object;
+	private:
+		// This function is meant to be only called by Object in AddComponent and nowhere else
+		virtual void Attach(const Pointer<Object>&) = 0;
+
+	public:
+		virtual ~Component() = default;
+	};
+
+	class ObjectComponent : public Component
 	{
+	private:
 		Reference<Object> Parent;
+
+		virtual void Attach(const Pointer<Object>&) override;
+	public:
+		virtual ~ObjectComponent() override = default;
 	};
 
 #ifdef USE_CUSTOM_GARBAGE_COLLECTION
--- object.cpp
+++ object.cpp
@@ -3,18 +3,47 @@
 template<class C>
 bool Engine::Object::HasComponentOfClass()
 {
-	// TODO: Add logic
+	type_assert(Engine::Component, C, "Class must be derived from Component");
+
 	return true;
 }
 
 template<class C>
 bool Engine::Object::AddComponentOfClass()
 {
+	type_assert(Engine::Component, C, "Class must be derived from Component");
+
 	if (HasComponentOfClass<C>())
 	{
 		return false;
 	}
-
-	// TODO: Add logic
+	
 	return true;
+}
+
+template<class C>
+void Engine::Object::RemoveComponentOfClass()
+{
+	type_assert(Engine::Component, C, "Class must be derived from Component");
+
+	for (size_t i = 0; i < Components.size(); i++)
+	{
+		if (dynamic_cast<C*>(Components[i].get()))
+		{
+			Components.erase(i);	// Why not call it remove???
+			return;
+		}
+	}
+}
+
+void Engine::Object::tick(float DeltaTime)
+{
+	Component* component;
+	for (size_t i = 0; i < Components.size(); i++)
+	{
+		component = Components[i].get();
+
+		component->tick(DeltaTime);
+	}
+	component = nullptr;
 }
