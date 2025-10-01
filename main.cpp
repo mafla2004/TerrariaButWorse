cdfb3e289dc16480e1ebbe658b462176dc51fb17
Mario Flaviano Caridi <mafla.caridi@gmail.com>
2025-10-01 17:32:58 +0200

    Implementations, bug fixing, testing the instantiation of smart pointers using aliases and wrapper functions

--- main.cpp
+++ main.cpp
@@ -3,6 +3,10 @@
 
 int main()
 {
+	using namespace Engine;
+
 	// Check correct creation of pointer to object
-	
+	Pointer<Object> ptr = MakePointer<Object>();
+
+	std::cout << "Object address: " << ptr << std::endl;
 }
