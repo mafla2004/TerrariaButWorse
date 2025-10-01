b41fa3d5e35df9ca498b4749f91d4891a571715a
Mario Flaviano Caridi <mafla.caridi@gmail.com>
2025-10-01 14:57:45 +0200

    Update on the remake of the engine

--- CoreEngine.h
+++ CoreEngine.h
@@ -67,18 +67,61 @@ class Engine
 	protected:
 		bool bShouldTick = true;
 
+		std::vector<Ownership<class Component>> components;
 	public:
 		inline Object() = default;
 		virtual ~Object() = default;
 
 		virtual void tick(float DeltaTime) = 0;
 
-		/* Checks if there */
+		// Checks if the class already owns a component of a certain class
 		template<class C>
 		bool HasComponentOfClass();
 
+		// Tries to add a component of a certain class, will fail if there is already a component
+		// of the same class attached to the object
 		template<class C>
 		bool AddComponentOfClass();
+
+		// Removes a component of a certain class
+		template<class C>
+		void RemoveComponentOfClass();
+
+		// Returns a reference (weak pointer) to a component of a certain class 
+		// attached to the object, null if there's none or if the component has been
+		// removed
+		template<class C>
+		Reference<C> GetComponentOfClass();
+	};
+
+	/*
+	*  An actor is an object that can be spawned in the game world
+	*/
+	class Actor : public Object
+	{
+	private:
+		Vector2 Position;
+		Vector2 Velocity;
+		double Rotation;	// In radians
+
+		Reference<Actor> Parent;
+		std::vector<Pointer<Actor>> Children;
+	public:
+		virtual ~Actor() = default;
+	};
+
+	// --------------------------------------------------
+	// COMPONENT FRAMEWORK
+	// --------------------------------------------------
+
+	/*
+	*	A component is a special object that is made to be attached to other objects.
+	*	Only its parent object can have an explicit pointer to it, this means that it's the
+	*	object's duty to call the tick functions of the components.
+	*/
+	class Component : public Object
+	{
+		Reference<Object> Parent;
 	};
 
 #ifdef USE_CUSTOM_GARBAGE_COLLECTION
